e371802775c5c228cabd61911db835fc0a30f82a
Bogdan Milescu <bogdan.milescu@softvision.ro>
2019-11-09 13:20:35 +0200

    added ami upgrade; added ami_spec tests

---
+++ spec/web_server/web_spec.rb
@@ -0,0 +1,8 @@
+describe service('apache2'), :if => os[:family] == 'ubuntu' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
